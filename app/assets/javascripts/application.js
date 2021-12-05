3daf4b661148700e3108dfd89c43741339755a7b
Francois Baumy <fr.baumy@gmail.com>
2021-12-05 22:30:41 +0100

    Add scroll messageges

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,12 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function () {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  }
+}
+
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
   $('.message .close')
@@ -26,5 +32,6 @@ $(document).on('turbolinks:load', function () {
         .transition('fade')
         ;
     });
+  scroll_bottom()
 })
 
