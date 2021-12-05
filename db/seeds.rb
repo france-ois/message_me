330606f1bcbbb1d909a411cd38e8c541d58c2896
Francois Baumy <fr.baumy@gmail.com>
2021-12-05 18:34:03 +0100

    Build User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Spark", password: "password")
+User.create(username: "Clothilde", password: "password")
+User.create(username: "Lino", password: "password")
+User.create(username: "Marc", password: "password")
+User.create(username: "Vincent", password: "password")
